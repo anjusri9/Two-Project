e149ef089dab4246ebf5607794a7201f2b60c5eb
anjusri9 <anjaniuppu@gmail.com>
2025-05-18 20:51:46 +0100

    pushing the updated chnages

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,15 +2,51 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH access"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
 resource "aws_instance" "iris_api" {
   ami           = var.ami
   instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  associate_public_ip_address = true 
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file(var.private_key_path)
+    host        = self.public_ip
+  }
 
   provisioner "remote-exec" {
     inline = [
       "sudo apt update",
-      "sudo apt install docker.io -y",
-      "docker run -d -p 5000:5000 anjusri9/iris_api:1.0.0"
+      "sudo apt install -y docker.io",
+      "sudo docker run -d -p 5000:5000 anjusri9/iris-api:1.0.0"
     ]
   }
+
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,6 @@
 variable "region" {}
 variable "ami" {}
 variable "instance_type" {}
+variable "private_key_path" {}
+variable "key_name" {}
+variable "vpc_id" {}
